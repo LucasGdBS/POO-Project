5a8035da5d0a75e2c5938d428d7ec7896521da5a
CarlosEdu <carlos.mec2005@gmail.com>
2023-10-10 15:44:18 -0300

    Classe ResultadoInclusaoVendedor

--- src/br/gov/cesarschool/poo/bonusvendas/negocio/ResultadoInclusaoVendedor.java
+++ src/br/gov/cesarschool/poo/bonusvendas/negocio/ResultadoInclusaoVendedor.java
@@ -1,5 +1,30 @@
+/*
+	ResultadoInclusaoVendedor
+	Pacote: br.gov.cesarschool.poo.bonusvendas.negocio
+	Atributos: devem ser privados
+	• numeroCaixaDeBonus – long
+	• mensagemErroValidacao – String
+	Construtor: público, deve inicializar os dois atributos.
+	Métodos: gets públicos para os dois atrubutos
+*/
+
 package br.gov.cesarschool.poo.bonusvendas.negocio;
 
 public class ResultadoInclusaoVendedor {
-
+	private long numeroCaixaDeBonus;
+	private String mensagemErroValidacao;
+	
+	public ResultadoInclusaoVendedor(long numeroCaixaDeBonus, String mensagemErroValidacao){
+		this.numeroCaixaDeBonus = numeroCaixaDeBonus;
+		this.mensagemErroValidacao = mensagemErroValidacao;
+	}
+	
+	public long getNumeroCaixaDeBonus() {
+		return numeroCaixaDeBonus;
+	}
+	
+	public String getMensagemErroValidacao() {
+		return mensagemErroValidacao;
+	}
+	
 }
