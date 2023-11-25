dcf8584211c9bca59fa294b91dabbcb4590e8a89
Lucas-PC <lgbs@cesar.school>
2023-11-25 18:54:49 -0300

    criação classe ComparadorVendedorRenda

---
+++ src/br/gov/cesarschool/poo/bonusvendas/negocio/ComparadorVendedorRenda.java
@@ -0,0 +1,31 @@
+package br.gov.cesarschool.poo.bonusvendas.negocio;
+
+import br.gov.cesarschool.poo.bonusvendas.entidade.Vendedor;
+import br.gov.cesarschool.poo.bonusvendas.util.Comparador;
+
+public class ComparadorVendedorRenda implements Comparador{
+	
+private static final ComparadorVendedorRenda instancia = new ComparadorVendedorRenda();
+	
+	private ComparadorVendedorRenda() {
+	}
+	
+	public static ComparadorVendedorRenda getInstance() {
+		return instancia;
+	}
+	
+	@Override
+	public int comparar(Object o1, Object o2) {		
+		Vendedor vendedor1 = (Vendedor) o1;
+        Vendedor vendedor2 = (Vendedor) o2;
+        
+        
+        if (vendedor1.getRenda() > vendedor2.getRenda()){
+        	return 1;
+        }else if (vendedor1.getRenda() == vendedor2.getRenda()) {
+        	return 0;
+        }
+        return -1;
+	}
+
+}
